1de472bed086e4fc1e035a043a152564e2329a4c
yassingamalz <yassen.gamal.yg@gmail.com>
2024-12-06 15:48:27 +0000

    init: Initial project setup with Spring Boot 3.4.0
    - Basic project structure
    - Core dependencies setup
    - Security configurations
    - Database configurations

---
+++ src/main/java/com/novavista/binaa/center/BinaaCenterApplication.java
@@ -0,0 +1,13 @@
+package com.novavista.binaa.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinaaCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinaaCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novavista/binaa/center/BinaaCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novavista.binaa.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinaaCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
